cd281cbaca3dd81909b01b1798e18486de55d9d4
AllieWu <44093216+AllieWu@users.noreply.github.com>
2020-09-13 22:08:50 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fa69f6e04f8ee08a6b74e8ca6a82b927.js"
+  "/precache-manifest.a50d11abdc372f9289a77b7a513e8d37.js"
 );
 
 self.addEventListener('message', (event) => {
